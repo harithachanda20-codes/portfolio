482ad74f86ac21a9f1c3e7b999504bb6db422e11
Chanda Haritha <harithachanda20@gmail.com>
2026-09-20 14:57:14 +0530

    Connect portfolio to live backend

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ if (contactForm) {
 
                 // Send data to Spring Boot backend
                 const response = await fetch(
-                    "http://localhost:8080/api/contact",
+                    "https://portfolio-backend-z3z0.onrender.com/api/contact",
                     {
                         method: "POST",
 
